e4052d1557097291415f2901b57dc479303c0f48
Santiago Donoso <sand@kea.dk>
2022-03-17 12:20:20 +0100

    ...

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-function showTweetModal(){
-  document.querySelector("#tweetModal").classList.remove("hidden")
+function toggleTweetModal(){
+  document.querySelector("#tweetModal").classList.toggle("hidden")
 }
 
-function hideTweetModal(){
-  document.querySelector("#tweetModal").classList.add("hidden")
-}
+// function showTweetModal(){
+//   document.querySelector("#tweetModal").classList.remove("hidden")
+// }
+
+// function hideTweetModal(){
+//   document.querySelector("#tweetModal").classList.add("hidden")
+// }
